56f04a67235e19b167c5849c3ea6afcee798e8a0
DKHSS <q526933815@sina.com>
2020-08-04 21:53:46 +0800

    :tada:init

---
+++ README.md
@@ -0,0 +1,7 @@
+#智障说话模拟器v0.1
+####安装依赖包
+```
+pip3 install -r requirements.txt
+```
+####用法
+python3 core.py
---
+++ core.py
@@ -0,0 +1,19 @@
+import morse
+from pypinyin import lazy_pinyin
+
+
+def main():
+    strings = input('请输入的内容：')
+    res = ''
+    for cur_word in lazy_pinyin(strings):
+        if cur_word.isalpha():
+            res += ''.join(morse.string_to_morse(cur_word)) + ' '
+        else:
+            res += cur_word
+    res = res.replace('.', '啊').replace('-', '吧')
+    print('小哑巴说：', res)
+
+
+if __name__ == '__main__':
+    while True:
+        main()
